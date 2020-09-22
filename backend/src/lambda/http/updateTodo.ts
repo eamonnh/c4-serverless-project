33bece152812ef5bbff92226e3a5624a31f4b03d
Eamonn Hegarty <eamonn_hegarty@hotmail.com>
2020-09-22 22:11:30 +0100

    UpdateTodo V1 working in Postman

--- backend/src/lambda/http/updateTodo.ts
+++ backend/src/lambda/http/updateTodo.ts
@@ -1,13 +1,57 @@
 import 'source-map-support/register'
+import { createLogger } from '../../utils/logger'
+import { DynamoDB } from 'aws-sdk';
+
+const todosTable = process.env.TODOS_TABLE
+const logger = createLogger('http')
+const docClient = new DynamoDB.DocumentClient();
 
 import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
 
 import { UpdateTodoRequest } from '../../requests/UpdateTodoRequest'
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  
   const todoId = event.pathParameters.todoId
   const updatedTodo: UpdateTodoRequest = JSON.parse(event.body)
 
-  // TODO: Update a TODO item with the provided id using values in the "updatedTodo" object
-  return undefined
+  try {
+
+        logger.info('Updating todo ',{todoId});
+
+        await docClient.update({
+          TableName: todosTable,
+          Key: { todoId },
+          UpdateExpression: 'set #name = :n, #dueDate = :due, #done = :d',
+          ExpressionAttributeValues: {
+              ':n': updatedTodo.name,
+              ':due': updatedTodo.dueDate,
+              ':d': updatedTodo.done
+          },
+          ExpressionAttributeNames: {
+              '#name': 'name',
+              '#dueDate': 'dueDate',
+              '#done': 'done'
+          }
+      }).promise();
+
+    // Return SUCCESS
+    logger.info('Update TODO Successful!', { todoId });
+    return {
+      statusCode: 204,
+      headers: {
+        'Access-Control-Allow-Origin': '*'
+      },
+      body: ''
+    }
+  }
+  catch (e) 
+  {
+    logger.error('Update TODO Failed!', { e });
+    return {
+      statusCode: 500,
+      body: e.message
+    }
+  }
+
 }
