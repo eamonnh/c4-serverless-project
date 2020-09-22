34f675b10e885d87f84ef384c95e2649cefa80ec
Eamonn Hegarty <eamonn_hegarty@hotmail.com>
2020-09-22 21:26:03 +0100

    DeleteTodo V1 working in Postman

--- backend/src/lambda/http/deleteTodo.ts
+++ backend/src/lambda/http/deleteTodo.ts
@@ -1,11 +1,42 @@
 import 'source-map-support/register'
+import { createLogger } from '../../utils/logger'
+import { DynamoDB } from 'aws-sdk';
 
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  
   const todoId = event.pathParameters.todoId
- 
+  const todosTable = process.env.TODOS_TABLE
+  const logger = createLogger('http')
+  const docClient = new DynamoDB.DocumentClient();
 
-  // TODO: Remove a TODO item by id
-  return undefined
+ 
+  try {
+
+    const params = {
+      TableName: todosTable,
+      Key: { todoId }
+    };
+
+    await docClient.delete(params).promise();
+
+    // Return SUCCESS
+    logger.info('Delete TODO Successful!', { todoId });
+    return {
+      statusCode: 204,
+      headers: {
+        'Access-Control-Allow-Origin': '*'
+      },
+      body: ''
+    }
+  }
+  catch (e) 
+  {
+    logger.error('Delete TODO Failed!', { e });
+    return {
+      statusCode: 500,
+      body: e.message
+    }
+  }
 }
