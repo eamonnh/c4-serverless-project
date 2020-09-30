1ae0a1ea25a632de5bc98c95471c91215cc1d32b
Eamonn Hegarty <eamonn_hegarty@hotmail.com>
2020-09-30 21:52:51 +0100

    generateUploadUrl V1 working in Postman

--- backend/src/lambda/http/generateUploadUrl.ts
+++ backend/src/lambda/http/generateUploadUrl.ts
@@ -1,10 +1,41 @@
 import 'source-map-support/register'
-
+import * as AWS from 'aws-sdk';
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+import { createLogger } from '../../utils/logger'
+
+const logger = createLogger('http')
+const urlExpiration = process.env.SIGNED_URL_EXPIRATION
+const bucket = process.env.ATTACHMENTS_S3_BUCKET
+const s3 = new AWS.S3({
+  signatureVersion: 'v4'
+})
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  const todoId = event.pathParameters.todoId
+  
+  try {
+    const todoId = event.pathParameters.todoId
 
-  // TODO: Return a presigned URL to upload a file for a TODO item with the provided id
-  return undefined
+    const url = s3.getSignedUrl('putObject', {
+      Bucket: bucket,
+      Key: todoId,
+      Expires: urlExpiration
+    })
+
+    //SUCCESS
+    logger.info('Url created', { url });
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*'
+      },
+      body: JSON.stringify({ url })
+    }
+  }
+  catch (e) {
+    logger.error('Url create Failed!', { e });
+    return {
+      statusCode: 500,
+      body: e.message
+    }
+  }
 }
